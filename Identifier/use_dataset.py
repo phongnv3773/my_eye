1020a6b05de4a91b4e2d3d07d86eb8e5c0aff65e
PhongnvPC <phongnv3773@gmail.com>
2018-05-20 16:28:29 +0700

    add big file

--- Identifier/use_dataset.py
+++ Identifier/use_dataset.py
@@ -11,7 +11,8 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 # TRAIN_DIR = dir_path + '/input/train/'
 TRAIN_DIR = dir_path + '/check_train/'
 print(TRAIN_DIR)
-TEST_DIR = dir_path+ '/check_test/'
+# TEST_DIR = dir_path+ '/check_test/'
+TEST_DIR = dir_path+ '/check_test2/'
 print(TEST_DIR)
 
 
@@ -19,9 +20,8 @@ print(TEST_DIR)
 # we only take the first 2000 from the training set
 # and only the first 1000 from the test set
 # REMOVE [0:2000] and [0:1000] when running locally
-train_image_file_names = [TRAIN_DIR+i for i in os.listdir(TRAIN_DIR)][0:2000] 
-test_image_file_names = [TEST_DIR+i for i in os.listdir(TEST_DIR)][0:707]
-
+train_image_file_names = [TRAIN_DIR+i for i in os.listdir(TRAIN_DIR)][0:10000] 
+test_image_file_names = [TEST_DIR+i for i in os.listdir(TEST_DIR)][0:447]
 
 # Slow, yet simple implementation with tensorflow
 # could be rewritten to be much faster
@@ -50,37 +50,16 @@ def decode_image(image_file_names, resize_func=None):
     return images
 
 
-
-train_images = decode_image(train_image_file_names)
-test_images = decode_image(test_image_file_names)
-all_images = train_images + test_images
-
-
-# Check mean aspect ratio (width/height), mean width and mean height
-width = []
-height = []
-aspect_ratio = []
-for image in all_images:
-    h, w, d = np.shape(image)
-    aspect_ratio.append(float(w) / float(h))
-    width.append(w)
-    height.append(h)
-
-
-# Free up some memory
-del train_images
-del test_images
-del all_images
-
-
 # WIDTH=500
 # HEIGHT=500
 # WIDTH=256
 # HEIGHT=256
-WIDTH=128
-HEIGHT=128
+# WIDTH=128
+# HEIGHT=128
 # WIDTH=64
 # HEIGHT=64
+WIDTH=28
+HEIGHT=28
 resize_func = lambda image: tf.image.resize_image_with_crop_or_pad(image, HEIGHT, WIDTH)
 
 
@@ -101,38 +80,38 @@ labels_tests = [[1., 0.] if 'dog' in name else [0., 1.] for name in test_image_f
 
 print(np.shape(labels))
 
-
-def add_layer(inputs, in_size, out_size, activation_function=None,):
+def add_layer(inputs, in_size, out_size, layer_name, activation_function=None, ):
     # add one more layer and return the output of this layer
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1,)
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, )
     Wx_plus_b = tf.matmul(inputs, Weights) + biases
     if activation_function is None:
         outputs = Wx_plus_b
     else:
-        outputs = activation_function(Wx_plus_b,)
+        outputs = activation_function(Wx_plus_b, )
     return outputs
 
 def compute_accuracy(v_xs, v_ys):
     global prediction
-    y_pre = sess.run(prediction, feed_dict={xs: v_xs})
+    y_pre = sess.run(prediction, feed_dict={xs_before_reshape: v_xs})
     correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
+    result = sess.run(accuracy, feed_dict={xs_before_reshape: v_xs, ys: v_ys})
     return result
 
 # define placeholder for inputs to network
-xs = tf.placeholder(tf.float32, shape=[None, 128 , 128 , 3]) # 
-xs1 = tf.reshape(xs, shape=[-1, 128 * 128 * 3])
+xs_before_reshape = tf.placeholder(tf.float32, shape=[None, 28 , 28 , 3]) # 
+xs = tf.reshape(xs_before_reshape, shape=[-1, 28 * 28 * 3])
 ys = tf.placeholder(tf.float32, shape=[None, 2])
 
 # add output layer
-prediction = add_layer(xs1,128*128*3 , 2,  activation_function=tf.nn.softmax)
+l1 = add_layer(xs, 28*28*3, 373, 'l1', activation_function=tf.nn.tanh)
+prediction = add_layer(l1,373, 2, 'l2', activation_function=tf.nn.relu)
+# prediction = add_layer(l1,373, 2, 'l2', activation_function=tf.nn.softmax)
 
 # the error between prediction and real data
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
-                                              reduction_indices=[1]))       # loss
-train_step = tf.train.GradientDescentOptimizer(0.3).minimize(cross_entropy)
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), reduction_indices=[1]))       # loss
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 sess = tf.Session()
 # important step
@@ -147,8 +126,8 @@ sess.run(init)
 for i in range(1000):
     # batch_xs, batch_ys = mnist.train.next_batch(100)
     # images, labels = iterator.get_next()
-    sess.run(train_step, feed_dict={xs: processed_train_images, ys: labels})
-    y_pre = sess.run(prediction, feed_dict={xs: processed_train_images})
+    sess.run(train_step, feed_dict={xs_before_reshape: processed_train_images, ys: labels})
+    # y_pre = sess.run(prediction, feed_dict={xs_before_reshape: processed_train_images})
     if i % 50 == 0:
         print(compute_accuracy(processed_test_images, labels_tests))
     
